fd9161dc6c2b43889344e3dd3343d700788f3344
Ezequiel Magi <ezequielmaggi@gmail.com>
2022-08-09 01:13:16 -0300

    finalizado formulario ejemplo no controlado

--- contactForm/src/components/DataInput.jsx
+++ contactForm/src/components/DataInput.jsx
@@ -1,9 +1,56 @@
-import React from 'react'
+import { useRef } from 'react'
 
 export const DataInput = () => {
+
+    
+    const formulario = useRef(null)
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+        const datos = new FormData(formulario.current)
+
+       // console.log(...datos.entries())
+
+       const objetoDatos = Object.fromEntries([...datos.entries()])
+
+       if(!objetoDatos.formName.trim()){
+        return console.log("vacio")
+       }
+
+       console.log("paso las validaciones!")
+       console.log(objetoDatos)
+       formulario.current.reset()
+
+
+    }
+
   return (
-    <form>
-        
+    <form onSubmit={handleSubmit} ref={formulario}>
+        <input 
+            type="text"
+            placeholder='Ingrese su nombre:' 
+            name="formName" 
+        />
+         <input 
+            type="text"
+            placeholder='Ingrese su apellido:' 
+            name="formLastName" 
+        />
+         <input 
+            type="text"
+            placeholder='Ingrese el curso:' 
+            name="formCurso" 
+        />
+        <select 
+            name="alumnoEstado"
+            defaultValue="Cursa"
+        >
+            <option value="cursa" >Cursa</option>
+            <option value="noCursa" >No Cursa</option>
+            
+        </select>
+        <button type='submit'>Agregar</button>
     </form>
   )
 }
--- contactForm/src/main.jsx
+++ contactForm/src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { ContactForm } from './ContactForm'
+import '../src/styles/main.css'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
