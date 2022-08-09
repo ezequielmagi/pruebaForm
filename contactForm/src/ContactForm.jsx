0a50b0b239006ebce88e57579617a95327762a9e
Ezequiel Magi <ezequielmaggi@gmail.com>
2022-08-09 00:21:58 -0300

    inicializando proyecto

---
+++ contactForm/src/ContactForm.jsx
@@ -0,0 +1,11 @@
+import { DataInput } from "./components/DataInput"
+
+
+export const ContactForm = () => {
+  return (
+    <>
+        <h1>Ingreso de contactos</h1>
+        <DataInput />
+    </>
+  )
+}
